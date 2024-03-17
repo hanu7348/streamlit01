b99e314ff08651d45f43172f4a75a4691a9a0712
Hanumanthu <104497776+hanu7348@users.noreply.github.com>
2024-03-17 22:41:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("ChatGPT-like ChatBot")
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 if "openai_model" not in st.session_state:
   st.session_state["openai_model"] = "gpt-3.5-turbo"
